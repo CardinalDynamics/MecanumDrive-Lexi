8024992546e62aae877268641b2e81b49c0df194
Aeris_Fox <aerothefox05@gmail.com>
2022-12-16 15:24:03 -0800

    define 550s or something

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,9 +12,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -27,16 +30,21 @@ public class Robot extends TimedRobot {
   private PWMSparkMax rearLeft = new PWMSparkMax(1);
   private PWMSparkMax frontRight = new PWMSparkMax(4);
   private PWMSparkMax rearRight = new PWMSparkMax(3);
- 
+
   private MecanumDrive drive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
   private XboxController controller = new XboxController(0);
+
+  private PWMSparkMax intakeLift = new PWMSparkMax(5);
+  private PWMSparkMax bunnyIntake = new PWMSparkMax(6);
+  private PWMSparkMax tubeIntake = new PWMSparkMax(2);
 
   // private PWMSparkMax motor5 = new PWMSparkMax(5);
   // private PWMSparkMax motor6 = new PWMSparkMax(6);
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
@@ -45,28 +53,43 @@ public class Robot extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
     drive.setDeadband(0.1);
-    // frontRight.setInverted(true);
-    // rearRight.setInverted(true);
+    frontRight.setInverted(true);
+    rearRight.setInverted(true);
+    SmartDashboard.putNumber("Front Left", frontLeft.get());
+    SmartDashboard.putNumber("Rear Left", rearLeft.get());
+    SmartDashboard.putNumber("Front Right", frontRight.get());
+    SmartDashboard.putNumber("Rear Right", rearRight.get());
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
    * chooser code above as well.
    */
   @Override
@@ -92,27 +115,34 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    drive.driveCartesian(controller.getRightX(), controller.getLeftX(), controller.getLeftY());
+    drive.driveCartesian(controller.getLeftY(), controller.getLeftX(), -controller.getRightX());
 
+    while (controller.getAButtonPressed) {
+      intakeLift.set(1);
+    }
 
   }
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   /** This function is called periodically during test mode. */
   @Override
@@ -122,9 +152,11 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 }
